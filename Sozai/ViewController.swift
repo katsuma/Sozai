c7cd149a880ec77fc7af9d141ce0e2c943ffe610
Ryo Katsuma <katsuma@cookpad.com>
2014-07-05 18:23:07 +0900

    Initial Commit

---
+++ Sozai/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sozai
+//
+//  Created by katsuma on 2014/07/05.
+//  Copyright (c) 2014年 katsuma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
