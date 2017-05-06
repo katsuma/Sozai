4f9bbe300d32c82986360606c04963ba19965a1e
Ryo Katsuma <katsuma@cookpad.com>
2017-05-06 15:25:19 +0900

    Use NetworkActivityIndicator

--- src/Component/SozaiDetail.js
+++ src/Component/SozaiDetail.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
-import { View, Image } from 'react-native';
+import { View, Image, StatusBar } from 'react-native';
 import SozaiStyle, { styles } from '../Style/SozaiStyle'
 
 export default class SozaiDetail extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      networkActivityIndicatorVisible: false,
+    };
+  }
+
+  _onChangeNetworkIndicatorVisible() {
+    this.setState({
+      networkActivityIndicatorVisible: !this.state.networkActivityIndicatorVisible,
+    });
+  };
+
   render() {
     return (
       <View style={{alignItems: 'stretch', flex: 1}}>
-        <Image style={{flex: 1, resizeMode: 'contain'}} source={{uri: this.props.url}} />
+        <StatusBar
+          networkActivityIndicatorVisible={this.state.networkActivityIndicatorVisible}
+        />
+        <Image
+          style={{flex: 1, resizeMode: 'contain'}}
+          source={{uri: this.props.url}}
+          onLoadStart={() => this._onChangeNetworkIndicatorVisible()}
+          onLoadEnd={() => this._onChangeNetworkIndicatorVisible()}
+        />
       </View>
     );
   }
